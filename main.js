8870fe13a3d5c2d24c5b1c9efb630dab72da4008
cristian038 <133276168+crisitian038@users.noreply.github.com>
2025-10-21 15:45:15 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('./sw.js')
             .then(reg => console.log('Registro SW exitoso: ', reg))
             .catch(err => console.error('Error de registro SW: ', err));
     });
+
 }
